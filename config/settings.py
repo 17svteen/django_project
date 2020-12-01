a6d17610363866b5b3eb5458f5c3dc090d8dddb8
17svt <17svt@17svt_@naver.com>
2020-12-01 21:34:30 +0900

    add debug true

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_d+fcl)@b%9&^475x+0pvph4x+0ln-!5)ccnj%(*aczrjglgdz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
